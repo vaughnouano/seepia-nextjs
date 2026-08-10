5bfc3d239da1c413ea3207757c2769a427b5c211
vaughnouano <vaughneurielouano@gmail.com>
2026-08-10 13:08:28 +0800

    refactor: transition to Router grouping folder system

---
+++ src/app/(public)/page.js
@@ -0,0 +1,9 @@
+import EquipmentCard from "../components/equipment/equipment-card";
+import HeroCard from "../components/equipment/hero-card";
+import { equipment } from "../lib/seepia-mock-data";
+
+export default function Home() {
+  const [heroItem, ...restOfEquipment] = equipment;
+
+  return;
+}
