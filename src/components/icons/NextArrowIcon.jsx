093eb548f142e13a376e27957f7c31172f366f0c
vaughnouano <vaughneurielouano@gmail.com>
2026-08-11 16:19:24 +0800

    feat: add multiple icon components

---
+++ src/components/icons/NextArrowIcon.jsx
@@ -0,0 +1,18 @@
+export default function NextArrowIcon() {
+  return (
+    <>
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M10.6667 25.3334L25.3334 16L10.6667 6.66669V25.3334Z"
+          fill="white"
+        />
+      </svg>
+    </>
+  );
+}
---
+++ src/components/icons/PreviousArrowIcon.jsx
@@ -0,0 +1,18 @@
+export default function PreviousArrowIcon() {
+  return (
+    <>
+      <svg
+        width="32"
+        height="32"
+        viewBox="0 0 32 32"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          d="M21.3333 25.3334L6.66663 16L21.3333 6.66669V25.3334Z"
+          fill="white"
+        />
+      </svg>
+    </>
+  );
+}
