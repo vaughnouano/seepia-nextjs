9ed1eec43964dcd73d7b0b09869702382b8a69d9
vaughnouano <vaughneurielouano@gmail.com>
2026-08-11 16:20:56 +0800

    feat: add global styles and layout component for consistent design

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,9 +1,33 @@
-import EquipmentCard from "../components/equipment/equipment-card";
-import HeroCard from "../components/equipment/hero-card";
-import { equipment } from "../lib/seepia-mock-data";
+import Button from "../../components/ui/button/Button";
+import Tag from "../../components/ui/tags/Tag";
+
+import HeartIcon from "../../components/icons/HeartIcon";
+import PreviousArrowIcon from "../../components/icons/PreviousArrowIcon";
+import NextArrowIcon from "../../components/icons/NextArrowIcon";
+import PaperPlane from "../../components/icons/PaperPlane";
 
 export default function Home() {
-  const [heroItem, ...restOfEquipment] = equipment;
-
-  return;
+  return (
+    <>
+      <Button variant="action" icon={<HeartIcon />}>
+        BOOK
+      </Button>
+      <br />
+      <Button variant="priceAction" price={500}>
+        Book
+      </Button>
+      <br />
+      <span>
+        <Button variant="nav" icon={<PreviousArrowIcon />}></Button>
+        <Button variant="nav" icon={<NextArrowIcon />}></Button>
+      </span>
+      <br />
+      <Button variant="action" icon={<PaperPlane />}>
+        Submit Form
+      </Button>
+      <br />
+      <Button variant="selector-wrap">1-2 day</Button>
+      <Button variant="selector-fill">Delivery</Button>
+    </>
+  );
 }
