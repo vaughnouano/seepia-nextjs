fc927249b7bdf5c8d636130158cd04be9cba6b32
vaughnouano <vaughneurielouano@gmail.com>
2026-09-04 13:38:29 +0800

    feat: update PriceButton component to accept onClick, type, and disabled props

--- src/components/ui/button/PriceButton/PriceButton.jsx
+++ src/components/ui/button/PriceButton/PriceButton.jsx
@@ -13,9 +13,15 @@ export default function PriceButton({
 
   // PriceButton specifics
   priceValue,
+  onClick,
+  type = "button",
+  disabled = false,
 }) {
   return (
     <button
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
       className={`${button[buttonState]} ${styles.button} ${fill ? styles.fill : styles.wrap}`}
     >
       <p className={styles.text}>{textContent}</p>
