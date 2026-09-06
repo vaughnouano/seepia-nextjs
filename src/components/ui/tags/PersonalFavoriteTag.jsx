2db3eef18ac06a572162e8ee6d426368cd343755
vaughnouano <vaughneurielouano@gmail.com>
2026-09-06 18:06:29 +0800

    refactor: remove unused tag components for cleaner codebase

--- src/components/ui/tags/PersonalFavoriteTag.jsx
+++
@@ -1,9 +0,0 @@
-import Styles from "./tag.module.css";
-
-export default function PersonalFavoriteTag() {
-  return (
-    <>
-      <div className={Styles.PersonalFavoriteTag}>Personal Favorite</div>
-    </>
-  );
-}
