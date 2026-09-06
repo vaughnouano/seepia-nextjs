53a9793ae210af4f45c1eede440e712cec01a876
vaughnouano <vaughneurielouano@gmail.com>
2026-09-06 18:06:17 +0800

    feat: restructure Home component layout and add styling for header and logo

--- src/app/(public)/page.js
+++ src/app/(public)/page.js
@@ -1,9 +1,32 @@
 import Styles from "./page.module.css";
 
+import CanonG7XMarkIIICard from "../../components/cameraCard/canonG7xMarkiiiCard";
+import DjiOsmoPocket4Card from "../../components/cameraCard/djiOsmoPocket4Card";
+import KodakFz55PixproCard from "@/src/components/cameraCard/kodakFz55PixproCard";
+
+import Image from "next/image";
+import seepiaLogo from "../../../public/images/seepia-rental-logo.png";
+
 export default function Home() {
   return (
-    <div className={Styles.container}>
-      <div></div>
+    <div className={Styles.Main}>
+      <div className={Styles.header}>
+        <Image
+          src={seepiaLogo}
+          className={Styles.SeepiaLogo}
+          alt="Seepia Large Logo"
+        />
+      </div>
+
+      <div className={Styles.container}>
+        <div className={Styles.heroSection}>
+          <CanonG7XMarkIIICard />
+        </div>
+        <div className={Styles.cards}>
+          <DjiOsmoPocket4Card />
+          <KodakFz55PixproCard />
+        </div>
+      </div>
     </div>
   );
 }
