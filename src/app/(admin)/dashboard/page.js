29407e91104a1492f4bdd3e0213af61e613aab11
vaughnouano <vaughneurielouano@gmail.com>
2026-09-09 22:35:14 +0800

    feat: implement dashboard page layout and add responsive styles for tablet view

--- src/app/(admin)/dashboard/page.js
+++ src/app/(admin)/dashboard/page.js
@@ -1,3 +1,9 @@
+import Styles from "./page.module.css";
+
 export default function dashboardPage() {
-  return <div>Dashboard page coming soon</div>;
+  return (
+    <>
+      <div className={Styles.Container}></div>
+    </>
+  );
 }
